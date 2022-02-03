14c3e84681b52002510136530a31fb99047ce84e
Hassan Inayat <50553966+hassan4567@users.noreply.github.com>
2022-02-03 18:09:42 +0500

    React native project setup

---
+++ App.js
@@ -0,0 +1,22 @@
+//================================ React Native Imported Files ======================================//
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+//================================ Local Imported Files ======================================//
+
+
+const App = () => {
+
+  return (
+      <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
+          <Text style={{color:'black'}}>DESEND APP</Text>
+      </View>
+  );
+};
+
+
+export default App;
